3d0ed535ac6fd82c2e4ceda672ae4a72e20c55e7
Denis <kryden1987@gmail.com>
2019-01-08 19:43:46 +0300

    sceleton create

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # nessi
 NES Simulator
+
+
+
+[Nesdev Wiki](http://wiki.nesdev.com/w/index.php/Nesdev_Wiki)
+
+
+[Marat Fayzullin](http://fms.komkon.org/)
---
+++ source/nessi/cli.hpp
@@ -0,0 +1,24 @@
+#ifndef CLI_HPP
+#define CLI_HPP
+
+
+namespace nessi
+  {
+
+  class cli
+    {
+    public:
+
+    private:
+
+    };
+
+
+
+
+  }
+
+
+
+
+#endif // CLI_HPP
